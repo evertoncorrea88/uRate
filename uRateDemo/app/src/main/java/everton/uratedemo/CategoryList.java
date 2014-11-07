b31b535c24993d784fa7ff2677caafa37f560cea
amarti65 <amarti65@kent.edu>
2014-11-07 00:28:28 -0500

    adding proposal

--- uRateDemo/app/src/main/java/everton/uratedemo/CategoryList.java
+++ uRateDemo/app/src/main/java/everton/uratedemo/CategoryList.java
@@ -52,7 +52,7 @@ public class CategoryList extends Activity {
         return true;
     }
 
-    @Override
+    @Override wor
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
